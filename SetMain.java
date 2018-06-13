a3dc94f0ce878bfc28d9d3a982dd94bd01a8c350
Cameron Beeler <cameron.beeler@gmail.com>
2018-06-13 13:59:27 -0700

    Incremental save

--- SetMain.java
+++ SetMain.java
@@ -38,6 +38,72 @@ public class SetMain {
         for (int i=0; i<100; i++){
             System.out.println(i+1 + " <-Index::Square-> " + sortedSquares.get(i) + " \tand Cubes-> " + sortedCubes.get(i));
         }
+        Set<String> words = new HashSet<>();
+        String sentence = "One day in the year of the fox";
+        String[] arraywords = sentence.split(" ");
+        words.addAll(Arrays.asList(arraywords));
 
+        for (String s:words){
+            System.out.println(s);
+        }
+        Set<String> nature = new HashSet<>();
+        Set<String> divine = new HashSet<>();
+        String[] natureWords = {"all", "nature", "is", "but", "art", "unknown", "to", "thee"};
+        nature.addAll(Arrays.asList(natureWords));
+
+        String[] divineWords = {"to", "err", "is", "human", "but", "to", "forgive", "is", "divine"};
+        divine.addAll(Arrays.asList(divineWords));
+
+        Set<String> diff1 = new HashSet<>(nature);
+        diff1.removeAll(divine);
+
+//  Asymmetric difference - the elements of one set - the elements of another (no union members)
+        Set<String> diff2 = new HashSet<>(divine);
+        diff2.removeAll(nature);
+        System.out.print("\nnature - divine:\t");
+        printSet(diff1);
+        System.out.print("\ndivine - nature:\t");
+        printSet(diff2);
+
+//  Symmetric difference is the union of two sets - remove the common elements
+//  Union is the whole join of all members of both sets
+//  Intersection are the elements in common from both sets
+//  Sets / HashSets do NOT allow duplication
+//  To obtain the Symmetric difference, we must acquire the Union and remove the Intersection
+
+        Set<String> Union = new HashSet<>(divine);
+        Union.addAll(nature);
+
+        Set<String> iSection = new HashSet<>(divine);
+        iSection.retainAll(nature);
+
+        Set<String> symDiff = new HashSet<>(Union) ;
+        symDiff.removeAll(iSection);
+        System.out.print("\nSymmetric Difference:\t");
+
+        printSet(symDiff);
+
+//  Inverse Symmetric difference
+        System.out.print("\nIntersection:\t");
+
+        printSet(iSection);
+
+//  containsAll is a non-destructive bulk method evaluation...
+        if(divine.containsAll(iSection)){
+            System.out.println("iSection is a subset of divine");
+        }
+        if (nature.containsAll(iSection)){
+            System.out.println("iSection is a subset of nature");
+        }
+
+    }
+
+
+    private static void printSet(Set<String> str){
+        for (String s:str){
+            System.out.print(s + " ");
+
+        }
+        System.out.println();
     }
 }
